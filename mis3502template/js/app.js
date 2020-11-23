25379f87df73b2adce46a18c31c8668b4b1df2cd
Nick Swink <nickswink7@gmail.com>
2020-11-23 16:16:26 -0500

    updating leaderboard test

--- mis3502template/js/app.js
+++ mis3502template/js/app.js
@@ -1,189 +1,200 @@
 "use strict";
 
 /* SOME CONSTANTS */
-var endpoint01 = "http://misdemo.temple.edu/auth";		// Change this to your own login api maybe ???
-var endpoint02 = "http://127.0.0.1:8220";				// api for game requests
+var endpoint01 = "http://misdemo.temple.edu/auth"; // Change this to your own login api maybe ???
+var endpoint02 = "http://127.0.0.1:8220"; // api for game requests
 localStorage.usertoken = 76;
-localStorage.lastnavlink = '';
+localStorage.lastnavlink = "";
 
 /* SUPPORTING FUNCTIONS */
 
-var navigationControl = function(the_link){
+var navigationControl = function (the_link) {
+  /* manage the content that is displayed */
+  var idToShow = $(the_link).attr("href");
+  localStorage.lastnavlink = idToShow;
 
-	/* manage the content that is displayed */
-	var idToShow = $(the_link).attr("href");
-	localStorage.lastnavlink = idToShow;
+  console.log(idToShow);
 
-	console.log(idToShow);
+  if (idToShow == "#div-login") {
+    /* what happens if the login/logout link is clicked? */
+    localStorage.usertoken = 0;
+    $(".secured").addClass("locked");
+    $(".secured").removeClass("unlocked");
+  }
 
-	if (idToShow == '#div-login' ){
-	/* what happens if the login/logout link is clicked? */
-		localStorage.usertoken = 0;
-		$(".secured").addClass("locked");
-		$(".secured").removeClass("unlocked");
-	}
-
-	$(".content-wrapper").hide(); 	/* hide all content-wrappers */
-	$(idToShow).show(); /* show the chosen content wrapper */
-	$("html, body").animate({ scrollTop: "0px" }); /* scroll to top of page */
-	$(".navbar-collapse").collapse('hide'); /* explicitly collapse the navigation menu */
-
-}; 
+  $(".content-wrapper").hide(); /* hide all content-wrappers */
+  $(idToShow).show(); /* show the chosen content wrapper */
+  $("html, body").animate({ scrollTop: "0px" }); /* scroll to top of page */
+  $(".navbar-collapse").collapse(
+    "hide"
+  ); /* explicitly collapse the navigation menu */
+};
 /* end navigation control */
 
-
-var getScores = function(){
-	let home1 = $("#calc-home").val();
-	let home2 = $("#calc-home2").val();
-	let food = $("#calc-food").val();
-	let shopping = $("#calc-shopping").val();
-	let travel1 = $("#calc-travel").val()
-	let travel2 = $("#calc-travel2").val();
-	let travel3 = $("#calc-travel3").val();
-// Convert to numbers and add sums
-	let home = Number(home1) + Number(home2);
-	let travel = Number(travel1) + Number(travel2) + Number(travel3);
-	food = Number(food);
-	shopping = Number(shopping);
-	let totalScore = home + travel + food + shopping;
-	var scores = {totalScore: totalScore, homeScore: home, travelScore: travel, foodScore: food, shoppingScore: shopping}; 
-	return scores;
+var getScores = function () {
+  let home1 = $("#calc-home").val();
+  let home2 = $("#calc-home2").val();
+  let food = $("#calc-food").val();
+  let shopping = $("#calc-shopping").val();
+  let travel1 = $("#calc-travel").val();
+  let travel2 = $("#calc-travel2").val();
+  let travel3 = $("#calc-travel3").val();
+  // Convert to numbers and add sums
+  let home = Number(home1) + Number(home2);
+  let travel = Number(travel1) + Number(travel2) + Number(travel3);
+  food = Number(food);
+  shopping = Number(shopping);
+  let totalScore = home + travel + food + shopping;
+  var scores = {
+    totalScore: totalScore,
+    homeScore: home,
+    travelScore: travel,
+    foodScore: food,
+    shoppingScore: shopping,
+  };
+  return scores;
 };
 
+// chart loading function
+var getChart = function () {
+  let scores = getScores();
+  let travel = scores.travelScore;
+  let home = scores.homeScore;
+  let food = scores.foodScore;
+  let shopping = scores.shoppingScore;
+  let total = scores.totalScore;
 
-
-// chart loading function 
-var getChart = function(){
-	let scores = getScores();
-	let travel = scores.travelScore;
-	let home = scores.homeScore;
-	let food = scores.foodScore;
-	let shopping = scores.shoppingScore;
-	let total = scores.totalScore;
-
-	var object = {
-		header: ["Name", "score"],
-		rows:[
-			["Travel",travel],
-			["Home",home],
-			["Food",food],
-			["Shopping",shopping],
-			["Total",total]
-		]
-	};
-	// create the chart
-	var chart = anychart.bar();
-	// add data
-	chart.data(object);
-	//set thick no spaces
-	chart.barGroupsPadding(0);
-	// draw
-	chart.container("chart");
-	chart.draw();
+  var object = {
+    header: ["Name", "score"],
+    rows: [
+      ["Travel", travel],
+      ["Home", home],
+      ["Food", food],
+      ["Shopping", shopping],
+      ["Total", total],
+    ],
+  };
+  // create the chart
+  var chart = anychart.bar();
+  // add data
+  chart.data(object);
+  //set thick no spaces
+  chart.barGroupsPadding(0);
+  // draw
+  chart.container("chart");
+  chart.draw();
 };
 // end of getChart
 
-
-
-var loadLeaderboard = function(){
-	
-	$("#leaderboard").html('');
-	$.ajax({
-		url: endpoint02 + '/leaderboard',
-		type: 'GET',
-		success: function(result){
-			for (let i = 0 ; i < result.length ; i++){
-				console.log(result[i]);
-				$("#leaderboard").append("<i class='fa fa-user'></i><li>" + result[i]['display_name'] + " : " + result[i]['total_score'] + "</li>")
-			};
-			
-		},
-		error: function(result){
-			console.log(result);
-			$("#leaderboard-error").addClass('alert alert-danger');
-			$("#leaderboard-error").html(result.status +''+ result.statusText +''+result.responseText);
-		}
-	});
+var loadLeaderboard = function () {
+  $("#leaderboard").html("");
+  $.ajax({
+    url: endpoint02 + "/leaderboard",
+    type: "GET",
+    success: function (result) {
+      for (let i = 0; i < result.length; i++) {
+        console.log(result[i]);
+        $("#leaderboard").append(
+          "<i class='fa fa-user'></i><li>" +
+            result[i]["display_name"] +
+            " : " +
+            result[i]["total_score"] +
+            "</li>"
+        );
+      }
+    },
+    error: function (result) {
+      console.log(result);
+      $("#leaderboard-error").addClass("alert alert-danger");
+      $("#leaderboard-error").html(
+        result.status + "" + result.statusText + "" + result.responseText
+      );
+    },
+  });
 };
 
+var submitNewTotal = function (display_name, totalScore) {
+  let the_serialized_data =
+    $("#calculator").serialize() +
+    "&userid=" +
+    localStorage.usertoken +
+    "&display_name=" +
+    display_name +
+    "&totalScore=" +
+    totalScore;
 
+  console.log(the_serialized_data);
 
-var submitNewTotal = function(display_name, totalScore){
-	let the_serialized_data = $("#calculator").serialize() + "&userid=" + localStorage.usertoken + "&display_name="+ display_name + "&totalScore=" + totalScore;
-
-	console.log(the_serialized_data);
-
-	$("#message").html('');
-	// send all the scores to total and receieve back a final score
-	$.ajax({
-		url: endpoint02 + '/total',
-		data: the_serialized_data,
-		type: 'POST',
-		success: function(result){
-			console.log(result);
-			$("#message").addClass('alert alert-success');
-			$("#message").html("You logged a waste score of " + totalScore + ". Check the breakdown on the dashboard!");
-		},
-		error: function(result){
-			//console.log(result);
-			$('#message').addClass('alert alert-danger');
-			$('#message').html(result.status +''+ result.statusText +''+result.responseText );
-		}
-	});
+  $("#message").html("");
+  // send all the scores to total and receieve back a final score
+  $.ajax({
+    url: endpoint02 + "/total",
+    data: the_serialized_data,
+    type: "POST",
+    success: function (result) {
+      console.log(result);
+      $("#message").addClass("alert alert-success");
+      $("#message").html(
+        "You logged a waste score of " +
+          totalScore +
+          ". Check the breakdown on the dashboard!"
+      );
+    },
+    error: function (result) {
+      //console.log(result);
+      $("#message").addClass("alert alert-danger");
+      $("#message").html(
+        result.status + "" + result.statusText + "" + result.responseText
+      );
+    },
+  });
 };
 
+var sendTip = function () {
+  // send tip to database with user specific id
+  let the_serialized_data =
+    $("#tip-form").serialize() + "&userid=" + localStorage.usertoken;
+  console.log(the_serialized_data);
 
-
-var sendTip = function(){
-	// send tip to database with user specific id
-	let the_serialized_data = $("#tip-form").serialize() + "&userid=" + localStorage.usertoken;
-	console.log(the_serialized_data);
-	
-	if($("#tip_message").val() == ''){
-		$('#tip-message').addClass('alert alert-danger');
-		$('#tip-message').html("Tip message cannot be blank");
-	}
-	else{
-		$.ajax({
-			url: endpoint02 + '/sendtip',
-			data: the_serialized_data,
-			type: 'POST',
-			success: function(result){
-				console.log(result);
-				$("#tip-message").addClass('alert alert-success');
-				$("#tip-message").html("Your tip was successfully submitted");
-			},
-			error: function(result){
-				console.log(result);
-				$('#tip-message').addClass('alert alert-danger');
-				$('#tip-message').html("An error occurred...");
-			}
-		});
-	}
+  if ($("#tip_message").val() == "") {
+    $("#tip-message").addClass("alert alert-danger");
+    $("#tip-message").html("Tip message cannot be blank");
+  } else {
+    $.ajax({
+      url: endpoint02 + "/sendtip",
+      data: the_serialized_data,
+      type: "POST",
+      success: function (result) {
+        console.log(result);
+        $("#tip-message").addClass("alert alert-success");
+        $("#tip-message").html("Your tip was successfully submitted");
+      },
+      error: function (result) {
+        console.log(result);
+        $("#tip-message").addClass("alert alert-danger");
+        $("#tip-message").html("An error occurred...");
+      },
+    });
+  }
 };
 
-var loadTipBoard = function(){
-
-	$.ajax({
-		url: endpoint02 + '/loadtips',
-		type: 'GET',
-		success: function(result){
-			console.log(result);
-			for (let tip = 0 ; tip < result.length ; tip++){
-				$("#tipboard").append("<li>" + result[tip].tip_message + "</li>");
-			}
-			
-		},
-		error: function(result){
-			console.log(result);
-			$('#tipboard').addClass('alert alert-danger');
-			$('#tipboard').html("An error occurred...");
-		}
-	})
-
-}
-
-
+var loadTipBoard = function () {
+  $.ajax({
+    url: endpoint02 + "/loadtips",
+    type: "GET",
+    success: function (result) {
+      console.log(result);
+      for (let tip = 0; tip < result.length; tip++) {
+        $("#tipboard").append("<li>" + result[tip].tip_message + "</li>");
+      }
+    },
+    error: function (result) {
+      console.log(result);
+      $("#tipboard").addClass("alert alert-danger");
+      $("#tipboard").html("An error occurred...");
+    },
+  });
+};
 
 //end of loadLeaderboard
 
@@ -214,118 +225,107 @@ var loginController = function(){
 // end of loginController
 */
 
-
 //document ready section
-$(document).ready(function (){
+$(document).ready(function () {
+  /* ------------------  basic navigation ----------------*/
 
-    /* ------------------  basic navigation ----------------*/
+  /* lock all secured content */
+  $(".secured").removeClass("unlocked");
+  $(".secured").addClass("locked");
 
-	/* lock all secured content */
-	$('.secured').removeClass('unlocked');
-	$('.secured').addClass('locked');
+  /* this reveals the default page */
+  $("#div-login").show();
+  $("#div-loginSideBar").show();
 
+  /* this controls navigation - show / hide pages as needed */
 
-    /* this reveals the default page */
-	$("#div-login").show();
-	$("#div-loginSideBar").show();
+  /* what to do when a navigation link is clicked */
+  $(".nav-link").click(function () {
+    navigationControl(this);
+  });
 
-    /* this controls navigation - show / hide pages as needed */
+  /* what happens if the login button is clicked? */
+  $("#btnLogin").click(function () {
+    //loginController();
 
-	/* what to do when a navigation link is clicked */
-	$(".nav-link").click(function(){
-		navigationControl(this);
-	});
+    // vvvvvvvvvvvvvvvvvvvvvv
+    $("#login_message").html("");
+    $("#login_message").hide();
+    $(".secured").removeClass("locked");
+    $(".secured").addClass("unlocked"); //TEMPORARY AUTO LOGIN BLOCK
+    $("#div-login").hide();
+    $("#div-loginSideBar").hide();
+    $("#div-splash").show();
+    // ^^^^^^^^^^^^^^^^^^^^^^^^
+  });
 
+  ////////////////////////////////////////////////////////
+  //START OF JS FOR DIV-SPLASH
 
-	/* what happens if the login button is clicked? */
-	$('#btnLogin').click(function(){
-		//loginController();
-		
-		// vvvvvvvvvvvvvvvvvvvvvv
-		$('#login_message').html('');					
-		$('#login_message').hide();						
-		$('.secured').removeClass('locked');
-		$('.secured').addClass('unlocked');				//TEMPORARY AUTO LOGIN BLOCK
-		$('#div-login').hide();
-		$('#div-loginSideBar').hide();
-		$('#div-splash').show();		
-		// ^^^^^^^^^^^^^^^^^^^^^^^^				
-	});
-	
-////////////////////////////////////////////////////////
-			//START OF JS FOR DIV-SPLASH
+  // Decrease your footprint navigation buttons
 
-// Decrease your footprint navigation buttons
+  $("#btnTravel").click(function () {
+    $("#home").hide();
+    $("#food").hide();
+    $("#shopping").hide();
+    $("#travel").show();
+  });
+  $("#btnHome").click(function () {
+    $("#home").show();
+    $("#food").hide();
+    $("#shopping").hide();
+    $("#travel").hide();
+  });
+  $("#btnFood").click(function () {
+    $("#home").hide();
+    $("#food").show();
+    $("#shopping").hide();
+    $("#travel").hide();
+  });
+  $("#btnShopping").click(function () {
+    $("#home").hide();
+    $("#food").hide();
+    $("#shopping").show();
+    $("#travel").hide();
+  });
 
-	$("#btnTravel").click(function() {
-		$('#home').hide();
-		$("#food").hide();
-		$("#shopping").hide();
-		$("#travel").show();
-	});
-	$("#btnHome").click(function() {
-		$('#home').show();
-		$("#food").hide();
-		$("#shopping").hide();
-		$("#travel").hide();
-	});
-	$("#btnFood").click(function() {
-		$('#home').hide();
-		$("#food").show();
-		$("#shopping").hide();
-		$("#travel").hide();
-	});
-	$("#btnShopping").click(function() {
-		$("#home").hide();
-		$("#food").hide();
-		$("#shopping").show();
-		$("#travel").hide();
-	});
+  // Load leaderboard here
+  loadLeaderboard();
 
+  //END OF JS FOR DIV-SPLASH
+  ////////////////////////////////////////////////////////
 
-// Load leaderboard here
-	loadLeaderboard();
+  ////////////////////////////////////////////////////////
+  //START OF JS FOR DIV-CALCULATOR
 
-			//END OF JS FOR DIV-SPLASH
-////////////////////////////////////////////////////////
+  // Submit button for calculator
 
+  $("#btnCalc").click(function () {
+    // Load information for submission
+    let totalScore = getScores().totalScore;
+    let display_name = $("#display_name").val();
 
-			
-////////////////////////////////////////////////////////
-			//START OF JS FOR DIV-CALCULATOR
+    //reload chart
+    $("#chart").html("");
+    getChart();
 
-	// Submit button for calculator
+    // Database operation
+    submitNewTotal(display_name, totalScore);
+  });
 
-	$("#btnCalc").click(function(){
-		// Load information for submission
-		let totalScore = getScores().totalScore;
-		let display_name = $("#display_name").val();
+  /* end the document ready event*/
 
-		//reload chart
-		$("#chart").html('');
-		getChart();
+  //END OF JS FOR DIV-CALCULATOR
+  ////////////////////////////////////////////////////////
 
-		// Database operation
-		submitNewTotal(display_name,totalScore);
-	});
+  ////////////////////////////////////////////////////////
+  //START OF JS FOR DIV-TIP
 
-/* end the document ready event*/
+  // load tips for the client
+  loadTipBoard();
 
-			//END OF JS FOR DIV-CALCULATOR
-////////////////////////////////////////////////////////
-
-
-
-////////////////////////////////////////////////////////
-			//START OF JS FOR DIV-TIP
-
-	// load tips for the client
-	loadTipBoard()
-
-	// what happens if you hit the submit tip button
-	$("#btnTip").click(function(){
-		sendTip();
-	});
-
-
-}); // closing document tab 
+  // what happens if you hit the submit tip button
+  $("#btnTip").click(function () {
+    sendTip();
+  });
+}); // closing document tab
--- nodejs/app.js
+++ nodejs/app.js
@@ -1,196 +1,194 @@
 "use strict";
-var express = require('express'); //import express, because I want easier management of get and post requests.  
-var bodyParser = require('body-parser'); //import body-parser, because I can't manage post data without it..
-var MySql = require('sync-mysql');  //sync-mysql is used to create a synchronous database connection
-const { type } = require('os');
-
+var express = require("express"); //import express, because I want easier management of get and post requests.
+var bodyParser = require("body-parser"); //import body-parser, because I can't manage post data without it..
+var MySql = require("sync-mysql"); //sync-mysql is used to create a synchronous database connection
+const { type } = require("os");
 
 //Database Connectivity
 const options = {
-  user: 'p14',
-  password: '39F8LY',
-  database: 'p14venus',
-  host: 'dataanalytics.temple.edu'
+  user: "p14",
+  password: "39F8LY",
+  database: "p14venus",
+  host: "dataanalytics.temple.edu",
 };
 
 // create the connection
 const connection = new MySql(options);
 
-var app = express();  //the express method returns an instance of a app object
-app.use(bodyParser.urlencoded({extended:false}));  //use this because incoming data is urlencoded
+var app = express(); //the express method returns an instance of a app object
+app.use(bodyParser.urlencoded({ extended: false })); //use this because incoming data is urlencoded
 
 // CORS Headers
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-    next();  //go process the next matching condition
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next(); //go process the next matching condition
 });
-
 
 //supporting functions go here
 
-
-//terminalWrite is the last supporting function to run.  It sends 
+//terminalWrite is the last supporting function to run.  It sends
 // output to the API consumer and ends the response.
 // This is hard-coded to always send a json response.
-var terminalWrite = function(res,Output,responseStatus){
-    res.writeHead(responseStatus, {'Content-Type': 'application/json'});
-	res.write(JSON.stringify(Output));
-    res.end();
+var terminalWrite = function (res, Output, responseStatus) {
+  res.writeHead(responseStatus, { "Content-Type": "application/json" });
+  res.write(JSON.stringify(Output));
+  res.end();
 };
 
-
 // ERROR TRAPPING FOR EVENT HANDLERS GO HERE //
-app.post('/sendtip', function(req,res,next){
-    //hand new tips that are sent
-    let tip_message = req.body.tip_message;
-    let user_id = req.body.userid;
-    
-    if(tip_message == undefined){
-        terminalWrite(res,"Request failed. No tip message provided.", 400);
-        return;
-    }
-    if(user_id == undefined){
-        terminalWrite(res,"Request failed. No user_id provided.", 400);
-        return;
-    }
-    if (isNaN(user_id)){
-        //400 error!
-        terminalWrite(res,"Request failed. Non-numeric user_id provided.",400);
-        return;
-    }
+app.post("/sendtip", function (req, res, next) {
+  //hand new tips that are sent
+  let tip_message = req.body.tip_message;
+  let user_id = req.body.userid;
 
-    // no errors move on
-    next();
+  if (tip_message == undefined) {
+    terminalWrite(res, "Request failed. No tip message provided.", 400);
+    return;
+  }
+  if (user_id == undefined) {
+    terminalWrite(res, "Request failed. No user_id provided.", 400);
+    return;
+  }
+  if (isNaN(user_id)) {
+    //400 error!
+    terminalWrite(res, "Request failed. Non-numeric user_id provided.", 400);
+    return;
+  }
+
+  // no errors move on
+  next();
 });
 
-app.post('/total', function(req,res,next){
-    let user_id = req.body.userid;
-    let display_name = req.body.display_name;
-    let totalScore = req.body.totalScore;
+app.post("/total", function (req, res, next) {
+  let user_id = req.body.userid;
+  let display_name = req.body.display_name;
+  let totalScore = req.body.totalScore;
 
-    if(user_id == undefined){
-        terminalWrite(res,"Request failed. No user_id provided.", 400);
-        return;
-    }
-    if (isNaN(user_id)){
-        //400 error!
-        terminalWrite(res,"Request failed. Non-numeric user_id provided.",400);
-        return;
-    }
-    if (display_name == undefined){
-        //400 error!
-        terminalWrite(res,"Request failed. No display name provided.",400);
-        return;
-    }
-    if (totalScore == undefined){
-        //400 error!
-        terminalWrite(res,"Request failed. No total score provided.",400);
-        return;
-    }
+  if (user_id == undefined) {
+    terminalWrite(res, "Request failed. No user_id provided.", 400);
+    return;
+  }
+  if (isNaN(user_id)) {
+    //400 error!
+    terminalWrite(res, "Request failed. Non-numeric user_id provided.", 400);
+    return;
+  }
+  if (display_name == undefined) {
+    //400 error!
+    terminalWrite(res, "Request failed. No display name provided.", 400);
+    return;
+  }
+  if (totalScore == undefined) {
+    //400 error!
+    terminalWrite(res, "Request failed. No total score provided.", 400);
+    return;
+  }
 
-    // no errors move on
-    next();
+  // no errors move on
+  next();
 });
-
-
-
-
-
 
 //app event handlers go here
 
+app.get("/", function (req, res) {
+  //what to do if request has no route ... show instructions
+  var message = [];
+  message[0] =
+    "POST to /total with home,home2,food,shopping,travel,travel2,travel3." +
+    "You will get back a total score";
+  message[1] = "GET to /leaderboard to get back the top 5 users and scores";
+  terminalWrite(res, message, 200);
+  return;
+});
 
-app.get('/', function(req, res) {
-    //what to do if request has no route ... show instructions
-    var message = [];
-    message[0] = "POST to /total with home,home2,food,shopping,travel,travel2,travel3." +
-                    "You will get back a total score";
-    message[1] = "GET to /leaderboard to get back the top 5 users and scores";
-    terminalWrite(res,message,200);
+app.get("/leaderboard", function (req, res) {
+  //return top 10 users and scores
+  try {
+    let results = connection.query(
+      "select display_name,total_score from leaderboard order by total_score desc limit 5;"
+    );
+    terminalWrite(res, results, 200);
     return;
+  } catch (e) {
+    console.log(e);
+    terminalWrite(res, "Internal server error", 500);
+    return;
+  }
 });
 
-app.get('/leaderboard', function(req,res){
-    //return top 10 users and scores
-    try {
-        let results = connection.query("select display_name,total_score from leaderboard order by total_score desc limit 5;");
-        terminalWrite(res,results,200);
-        return;
-    } 
-    catch (e) {
-        console.log(e);
-        terminalWrite(res,"Internal server error",500);
-        return;
-    };
+app.post("/total", function (req, res) {
+  // handle post requests to total route
+  var user_id = req.body.userid;
+  var display_name = req.body.display_name;
+  var totalScore = req.body.totalScore;
+  console.log("User:", user_id, "logged score of:", totalScore);
+
+  try {
+    let sqlTxtDelete = "delete from leaderboard where user_id = ?;";
+    let sqlTxtInsert =
+      "insert into leaderboard (user_id,display_name,total_score) values (?, ?, ?);";
+    //Delete current record and insert new one every time
+    var x = connection.query(sqlTxtDelete, [user_id]);
+    var results = connection.query(sqlTxtInsert, [
+      user_id,
+      display_name,
+      totalScore,
+    ]);
+    terminalWrite(res, results, 200);
+    return;
+  } catch (e) {
+    console.log("Internal server error", e);
+    terminalWrite(res, e, 500);
+    return;
+  }
 });
 
-app.post('/total', function(req,res){
-    // handle post requests to total route
-    var user_id = req.body.userid;
-    var display_name = req.body.display_name;
-    var totalScore = req.body.totalScore;
-    console.log("User:",user_id,"logged score of:",totalScore);
+app.post("/sendtip", function (req, res) {
+  //hand new tips that are sent
+  var tip_likes = 0;
+  var tip_message = req.body.tip_message;
+  var user_id = req.body.userid;
+  var sqlTxt =
+    "insert into tips (tip_message, tip_likes, user_id) values (?, ?, ?);";
 
-    try {
-        let sqlTxtDelete = "delete from leaderboard where user_id = ?;";        
-        let sqlTxtInsert = "insert into leaderboard (user_id,display_name,total_score) values (?, ?, ?);";
-        //Delete current record and insert new one every time
-        var x = connection.query(sqlTxtDelete,[user_id]);
-        var results = connection.query(sqlTxtInsert,[user_id,display_name,totalScore]);
-        terminalWrite(res,results,200);
-        return;
-    } 
-    catch (e) {
-        console.log("Internal server error", e);
-        terminalWrite(res,e,500);
-        return;
-    }
+  try {
+    let results = connection.query(sqlTxt, [tip_message, tip_likes, user_id]);
+    console.log("User", user_id, "logged tip:", tip_message);
+    terminalWrite(res, results, 200);
+    return;
+  } catch (e) {
+    console.log(e);
+    terminalWrite(res, "An internal server error has occurred...", 500);
+    return;
+  }
 });
 
-app.post('/sendtip', function(req,res){
-    //hand new tips that are sent
-    var tip_likes = 0;
-    var tip_message = req.body.tip_message;
-    var user_id = req.body.userid;
-    var sqlTxt = "insert into tips (tip_message, tip_likes, user_id) values (?, ?, ?);";
-    
-    try {
-        let results = connection.query(sqlTxt,[tip_message,tip_likes,user_id]);
-        console.log("User",user_id,"logged tip:",tip_message);
-        terminalWrite(res,results,200);
-        return;
-    } catch (e) {
-        console.log(e);
-        terminalWrite(res,"An internal server error has occurred...",500);
-        return;
-    }
+app.get("/loadtips", function (req, res) {
+  // get tips for tipboard
+  let sqlTxt =
+    "select tip_message, tip_likes, date_created from tips order by date_created;";
+
+  try {
+    let results = connection.query(sqlTxt);
+    terminalWrite(res, results, 200);
+    return;
+  } catch (e) {
+    console.log(e);
+    terminalWrite(res, "An internal server error has occurred...", 500);
+    return;
+  }
 });
 
-app.get('/loadtips', function(req,res){
-    // get tips for tipboard
-    let sqlTxt = "select tip_message, tip_likes, date_created from tips order by date_created;";
-
-    try {
-        let results = connection.query(sqlTxt);
-        console.log(results);
-        terminalWrite(res,results,200);
-        return;
-    } catch (e) {
-        console.log(e);
-        terminalWrite(res,"An internal server error has occurred...",500);
-        return
-    }
-});
-
-
-
-//This piece of code creates the server  
+//This piece of code creates the server
 //and listens for a request on a port
-//we are also generating a console message once the 
+//we are also generating a console message once the
 //server is created
-var server = app.listen(8220, function(){
-    var port = server.address().port;
-    console.log("The server is listening on port:" + port);
+var server = app.listen(8220, function () {
+  var port = server.address().port;
+  console.log("The server is listening on port:" + port);
 });
